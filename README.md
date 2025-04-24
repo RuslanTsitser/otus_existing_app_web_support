e0e7b5a1966e0fa3d81840cdb9c03d095749ca9e
RuslanTsitser <cicer_ruslan@mail.ru>
2025-04-24 21:24:51 +0300

    hello

--- README.md
+++
@@ -1,3 +0,0 @@
-# otus_existing_app_web_support
-
-A new Flutter project.
--- android/app/src/main/kotlin/com/tsitser/dev/otus_existing_app_web_support/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.tsitser.dev.otus_existing_app_web_support
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
