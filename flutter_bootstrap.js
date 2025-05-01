2c97b6ba0b394a10ef58891d17d58618dc30911b
RuslanTsitser <cicer_ruslan@mail.ru>
2025-05-01 20:09:12 +0300

    Updates

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"18b71d647a292a980abb405ac7d16fe1f0b204
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "472438187"
+    serviceWorkerVersion: "4106190863"
   }
 });
